5e39adaf463166463eed8af92eb30201d7a6b315
JuanCa <juanca_20201@outlook.es>
2024-09-03 21:34:04 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Dragon_Ball_Z_App',
+  webDir: 'www'
+};
+
+export default config;
